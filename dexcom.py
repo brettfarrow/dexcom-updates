2119661e0fcb7dc44917ba06ba000eafd4fe4406
Brett Farrow <brett@acorncity.com>
2022-07-02 13:04:49 -0600

    End of Episode 1

--- dexcom.py
+++ dexcom.py
@@ -1,22 +1,71 @@
 from decouple import config
 from pydexcom import Dexcom
 from twilio.rest import Client
+import logging
+from logging.handlers import RotatingFileHandler
 
-dexcom = Dexcom(
-    config("DEXCOM_USERNAME"),
-    config("DEXCOM_PASSWORD")
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
 )
-bg = dexcom.get_current_glucose_reading()
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+logger = logging.getLogger(__name__)
+handler = RotatingFileHandler(
+    'dexcom.log', maxBytes=1000000, backupCount=3)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
-if bg.value <= 80:
-    result = f"blood sugar is {bg.value}, trend is {bg.trend_description} ({bg.trend_arrow}) at {bg.time}"
 
-    client = Client(config("TWILIO_ACCOUNT"), config("TWILIO_TOKEN"))
+def should_send_message(dex):
+    if dex.value <= 80:
+        return True
+    if dex.value <= 100 and dex.trend_description == "falling":
+        return True
+    if dex.trend_description == "falling quickly":
+        return True
+    if dex.trend_description == "rising quickly":
+        return True
 
-    message = client.messages.create(
-        to=config("TWILIO_TO_NUMBER"),
-        from_=config("TWILIO_FROM_NUMBER"),
-        body=result
+    return False
+
+
+def build_message_body(dex):
+    return (
+        f"Blood sugar update:\n"
+        f"Current reading is {dex.value}\n"
+        f"Trend is {dex.trend_description} ({dex.trend_arrow})\n"
+        f"Reading taken at {dex.time}"
     )
-else:
-    print("All is clear for now")
+
+
+def main():
+    try:
+        dexcom = Dexcom(
+            config("DEXCOM_USERNAME"),
+            config("DEXCOM_PASSWORD")
+        )
+        bg = dexcom.get_current_glucose_reading()
+
+        if should_send_message(bg):
+            result = build_message_body(bg)
+            print(result)
+
+            client = Client(config("TWILIO_ACCOUNT"), config("TWILIO_TOKEN"))
+
+            message = client.messages.create(
+                to=config("TWILIO_TO_NUMBER"),
+                from_=config("TWILIO_FROM_NUMBER"),
+                body=result
+            )
+            logger.info("Sending message, result:", message)
+        else:
+            result = build_message_body(bg)
+            logger.info("Did not meet condition to send, exiting successfully")
+    except BaseException as err:
+        logger.error(f"Unexpected {err=}, {type(err)=}")
+        raise
+
+
+if __name__ == "__main__":
+    main()
