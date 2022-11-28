cca6c9d71347f5d92b683a1f6076dd30bf5d6cfd
Brett Farrow <brett@acorncity.com>
2022-11-27 23:40:41 -0700

    End of Episode 2

--- dexcom.py
+++ dexcom.py
@@ -3,6 +3,7 @@ from pydexcom import Dexcom
 from twilio.rest import Client
 import logging
 from logging.handlers import RotatingFileHandler
+from datetime import datetime
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
@@ -17,37 +18,59 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-def should_send_message(dex):
-    if dex.value <= 80:
-        return True
-    if dex.value <= 100 and dex.trend_description == "falling":
-        return True
-    if dex.trend_description == "falling quickly":
-        return True
-    if dex.trend_description == "rising quickly":
-        return True
+def should_send_message(dex, time):
+    time_result, data_result = False, False
 
-    return False
+    try:
+        if (time - dex.time).total_seconds() < 60:
+            time_result = True
+
+        if dex.value <= 80:
+            data_result = True
+        if dex.value <= 100 and dex.trend_description == "falling":
+            data_result = True
+        if dex.trend_description == "falling quickly":
+            data_result = True
+        if dex.trend_description == "rising quickly":
+            data_result = True
+    except:
+        if dex == None and time.minute % 15 == 0:
+            return True
+
+    return time_result and data_result
+
+
+def should_make_call(dex, time):
+    time_result, data_result = True, False
+
+    if (time - dex.time).total_seconds() < 60:
+        time_result = True
+    if dex.value <= 55 or dex.value >= 300:
+        data_result = True
+
+    return time_result and data_result
 
 
 def build_message_body(dex):
+    reading_time = dex.time.strftime("%I:%M %p on %B %d")
     return (
         f"Blood sugar update:\n"
         f"Current reading is {dex.value}\n"
         f"Trend is {dex.trend_description} ({dex.trend_arrow})\n"
-        f"Reading taken at {dex.time}"
+        f"Reading taken at {reading_time}"
     )
 
 
 def main():
     try:
+        start_time = datetime.now()
         dexcom = Dexcom(
             config("DEXCOM_USERNAME"),
             config("DEXCOM_PASSWORD")
         )
         bg = dexcom.get_current_glucose_reading()
 
-        if should_send_message(bg):
+        if should_send_message(bg, start_time):
             result = build_message_body(bg)
             print(result)
 
@@ -58,10 +81,25 @@ def main():
                 from_=config("TWILIO_FROM_NUMBER"),
                 body=result
             )
-            logger.info("Sending message, result:", message)
+            # Use result for just the SMS, message for Twilio API response
+            logger.info(f"Sending message, result: {message}")
         else:
-            result = build_message_body(bg)
             logger.info("Did not meet condition to send, exiting successfully")
+
+        if should_make_call(bg, start_time):
+            try:
+                client
+            except NameError:
+                client = Client(config("TWILIO_ACCOUNT"),
+                                config("TWILIO_TOKEN"))
+
+            client.calls.create(
+                url=config("TWIML_LOCATION_URL"),
+                to=config("TWILIO_TO_NUMBER"),
+                from_=config("TWILIO_FROM_NUMBER"),
+            )
+        else:
+            logger.info("Did not meet condition to call, exiting successfully")
     except BaseException as err:
         logger.error(f"Unexpected {err=}, {type(err)=}")
         raise
